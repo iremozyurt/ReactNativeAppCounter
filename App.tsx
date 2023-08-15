e861dde09ffa7677bc35ec66c3200ac16124aa82
irem.ozyurt <iremel2211@hotmail.com>
2023-08-15 13:30:57 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const onClickHandler = () => {
+    setCounter(counter + 1)
+  }
+  return (
+    <View style={styles.body}>
+
+      <Text style={styles.text}>{counter * 5}</Text>
+      <Button title='Add' onPress={onClickHandler}></Button>
+      <Text style={styles.text}>You clicked {counter}times</Text>
+    </View>
+
+
+  );
+};
+
+const styles = StyleSheet.create({
+
+  body: {
+    flex: 1,
+    backgroundColor: '#0000ff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 10,
+  },
+});
+
+
+
+export default App;
